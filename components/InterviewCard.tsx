87ec704fa7fe5c18b6990c62785e11ef8fd9c2d6
ngnamquoc <nguyennamquoc.mar@gmail.com>
2025-04-27 23:29:57 -0400

    feau(feedback): display feedback preview for taken interview in main dashboard

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.actions";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -13,7 +14,7 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback = userId && id ? await getFeedbackByInterviewId({interviewId:id,userId}):null
   const normalizedType = /mix/gi.test(type) ? "mixed" : type;
   const formattedDay = dayjs(
     feedback?.createdAt || createdAt || Date.now()
